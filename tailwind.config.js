fbee42861fb8d95715088809f3562f83c3642fe9
Ibranista <ibranista88@gmail.com>
2022-12-27 12:24:49 +0100

    fixed linters.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/*.js",
+    "./components/*.js",
+    "./components/**/*.js",
+    "./pages/**/*.js",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/*{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
