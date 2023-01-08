54c52bf277b1454cbad11bce4b23a8487afe32b6
Ibranista <ibranista88@gmail.com>
2023-01-08 20:10:54 +0100

    fixed hydration error and added background animation.

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,43 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 // import framermotion
 import { motion } from "framer-motion";
 import { autoPlacement } from "@floating-ui/core";
+import { useEffect } from "react";
+
+// create readmorereadless component
+function ReadMoreReadLess({ children, maxCharCount = 100 }) {
+  const text = children;
+  const [isTruncated, setIsTruncated] = useState(true);
+  const resultString = isTruncated ? text.slice(0, maxCharCount) : text;
+  const toggleIsTruncated = () => {
+    setIsTruncated(!isTruncated);
+  };
+  useEffect(() => {
+    setIsTruncated(true);
+  }, []);
+
+  return (
+    <p>
+      {resultString}
+      {text.length > maxCharCount && (
+        <span
+          onClick={toggleIsTruncated}
+          className="text-blue-500 hover:cursor-pointer"
+        >
+          {isTruncated ? " ...read more" : " read less"}
+        </span>
+      )}
+    </p>
+  );
+}
+
 function Card(props) {
   return (
     <>
+      {/* use readmorereadless component here */}
+
       <div
-        className="relative z-[800] pb-3 bg-[#2C3A50]  w-[250px] sm:w-[300px]  items-center rounded-sm top-20 text-white font-Inter leading-8 hover:bg-blue-900
-      border-b-2 border-white border-4 hover:shadow-lg
+        className="relative z-[800] pb-3  w-[250px] sm:w-[300px]  items-center top-20 text-black hover:text-white font-Inter leading-8 hover:bg-slate-800
+      border-b-2 border-white hover:border-blue-900 border-4 hover:shadow-2xl  h-[460px] rounded-lg
+      overflow-auto scroll-smooth scroll-p-1 transition-all duration-300 cursor-pointer ease-linear
       "
       >
-        <Link href={props.href}>
-          <Image
-            src={props.imgUrl}
-            alt={props.name}
-            cover
-            height={160}
-            width={300}
-            className="relative"
-          />
-          <div className="box-wrapper px-5 pt-3">
-            <h1 className="font-bold text-2xl mb-2">Luxuria Hotel</h1>
+        <Image
+          src={props.imgUrl}
+          alt={props.name}
+          height={160}
+          width={300}
+          className="relative"
+        />
+        <div className="box-wrapper px-5 pt-3">
+          <h1 className="font-bold text-2xl mb-2">{props.name}</h1>
 
-            <h3 className="font-semibold"> An elegant 5 ⭐ hotel </h3>
-            <p className="text-left">
-              located in the heart of the city, With over 500 elegantly
-              decorated guest rooms, a full-service spa, and several gourmet
-              dining options
-            </p>
+          <h3 className="font-semibold">{props.star} </h3>
+          <div className="text-left">
+            <ReadMoreReadLess maxCharCount={100}>
+              {props.description}
+            </ReadMoreReadLess>
           </div>
-          <div className="button-wrapper flex justify-around overflow-hidden mt-3">
+        </div>
+        <div className="button-wrapper">
+          <Link href={props.href}>
             <motion.button
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 1 }}
               initial={{ scale: 0.9 }}
               animate={{ scale: 1.1 }}
-              className="bg-[#D9D9D9] px-2 ml-2 rounded-sm text-[#3E649E]
+              className="text-[#D9D9D9] bg-black px-2 rounded-sm 
                 hover:shadow-blue-900 hover:border-blue-800 hover:text-white hover:bg-[#2C3A50]
                 shadow-lg
                 border-b-2
@@ -50,7 +81,7 @@ function Card(props) {
               whileTap={{ scale: 1 }}
               initial={{ scale: 0.9 }}
               animate={{ scale: 1.1 }}
-              className="bg-[#D9D9D9] px-2 rounded-sm text-[#3E649E]
+              className="text-[#D9D9D9] bg-black px-2 rounded-sm
                 hover:shadow-blue-900 hover:border-blue-800 hover:text-white hover:bg-[#2C3A50]
                 shadow-lg
                 border-b-2
@@ -58,8 +89,8 @@ function Card(props) {
             >
               $150 per day
             </motion.button>
-          </div>
-        </Link>
+          </Link>
+        </div>
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,11 @@ import Hero from "../components/Hero";
 import Banner from "../components/banner";
 import Image from "next/image";
 import Card from "../components/Card";
-// fetcher method
+import hotels from "../data/hotels.json";
 
 export async function getStaticProps(context) {
-  // const data = fetch()
   return {
-    props: {}, // will be passed to the page component as props
+    props: { hotels }, // will be passed to the page component as props
   };
 }
 
@@ -21,36 +20,42 @@ export default function Home(props) {
       <div className="font-Inter">
         <Head>
           <title>Hotels Near Me</title>
-          <link rel="stylesheet" href="/favicon.ico" />
         </Head>
-        <main className="relative bg-banner">
+        <main className="relative bg-banner bg-fixed">
           <Banner />
           <section
             className="
      bg-hero-pattern sm:bg-desktop-pattern relative z-[80] w-full -top-72 flex gap-5
      items-center justify-center pt-10 sm:pt-5 pb-32 px-5 flex-wrap
+     bg-gray-100
      "
           >
-            <Card
-              name="Hotel 1"
-              imgUrl="/HotelBanner.jpg"
-              href="/details/crown-hotel"
-            />
-            <Card
-              name="Hotel 1"
-              imgUrl="/HotelBanner.jpg"
-              href="/details/crown-hotel"
-            />
-            <Card
-              name="Hotel 1"
-              imgUrl="/HotelBanner.jpg"
-              href="/details/crown-hotel"
-            />
-            <Card
-              name="Hotel 1"
-              imgUrl="/HotelBanner.jpg"
-              href="/details/crown-hotel"
-            />
+            {props.hotels.map((hotel) => {
+              return (
+                <Card
+                  key={hotel.id}
+                  name={hotel.name}
+                  imgUrl={hotel.imgUrl}
+                  href={`/details/${hotel.id}`}
+                  description={hotel.description}
+                  alt={hotel.alt}
+                  star={hotel.star}
+                />
+              );
+            })}
+            <div className="custom-shape-divider-top-1673200510">
+              <svg
+                data-name="Layer 1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1200 120"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                  className="shape-fill"
+                ></path>
+              </svg>
+            </div>
           </section>
         </main>
         <Hero />
