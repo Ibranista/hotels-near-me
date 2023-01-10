59607e35bff01d3d0812ac4f8f8e8e57fe66e12c
Ibranista <ibranista88@gmail.com>
2023-01-10 03:22:11 +0100

    added fallback value.

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -4,29 +4,6 @@ import { useRouter } from "next/router";
 import hotelsData from "../../data/hotels.json";
 import Head from "next/head";
 export async function getStaticProps(staticProps) {
-  // fsq3c2EgkBLcOaok8uikpCe3W6wMHenhNoCCcMoa9myVAYU=
-  // fetch coffee stores from foursquare.com using client_id=1JZVCUEW05WWSSQ5G2YU02BGW1VXQYTNDTTLWZXDVIZ2HGMM and client_secret=O4QFPRLTTFREYYHKO2BJRVRTKZRCNNFCD0RP3KXAQ420VDNS and ll=40.7,-74 and v=20180323 and use version 3 of the API
-  let url = "https://api.foursquare.com/v3/places/{fsq_id}";
-  // const sdk = require("api")("@fsq-developer/v1.0#17kgj534lcjp10ly");
-
-  const authToken = "fsq3c2EgkBLcOaok8uikpCe3W6wMHenhNoCCcMoa9myVAYU=";
-
-  const placeId = "5a187743ccad6b307315e6fe";
-
-  const headers = {
-    Authorization: `Bearer ${authToken}`,
-  };
-
-  const body = {
-    fsq_id: placeId,
-  };
-
-  fetch(
-    "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&&query=coffee stores&client_id=1JZVCUEW05WWSSQ5G2YU02BGW1VXQYTNDTTLWZXDVIZ2HGMM&client_secret=O4QFPRLTTFREYYHKO2BJRVRTKZRCNNFCD0RP3KXAQ420VDNS&v=20180323"
-  )
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-  console.log("first");
   // One of the properties of the staticProps object is params, which is an object that contains the route parameters for the page being rendered.
   const params = staticProps.params;
   return {
