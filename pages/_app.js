f04d6d766f0913400c5bc222d43194afe6afee6f
Ibranista <ibranista88@gmail.com>
2023-01-11 10:29:28 +0100

    edit opacity.

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,7 @@ export default function App({ Component, pageProps }) {
   }
   return (
     <motion.div
-      initial={{ opacity: 0 }}
+      initial={{ opacity: 0.7 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 1 }}
